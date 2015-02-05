f2e2a779d56e19b76465444f7e74723e775df92b
Robert Rees <robert.rees@guardian.co.uk>
2015-02-05 10:48:15 +0000

    Adds the basic dependencies

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject article-tag-checker "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+  	[hickory "0.5.4"]
+  	[yesql "0.4.0"]
+  	])
---
+++ test/article_tag_checker/core_test.clj
@@ -0,0 +1,7 @@
+(ns article-tag-checker.core-test
+  (:require [clojure.test :refer :all]
+            [article-tag-checker.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
