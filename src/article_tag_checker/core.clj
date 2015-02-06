bb8e5d85fea47ab61f1872024531336a0aece61c
Robert Rees <robert.rees@guardian.co.uk>
2015-02-06 10:34:44 +0000

    Adds what I think are the valid tags that Scribe accepts

--- src/article_tag_checker/core.clj
+++ src/article_tag_checker/core.clj
@@ -1,5 +1,11 @@
 (ns article-tag-checker.core)
 
+(def valid-scribe-tags
+	[:code :strong :b :em :i :strike :a :ul :ol :li :blockquote :h2 :sub :sup])
+(def valid-scribe-attributes {:a [:href] :blockquote :class})
+
+(def analysis (atom {}))
+
 (defn foo
   "I don't do a whole lot."
   [x]
