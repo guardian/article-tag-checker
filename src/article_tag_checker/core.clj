b5b0200f28d2b68bfa082d4bd8160df6b5cb8ea9
Robert Rees <robert.rees@guardian.co.uk>
2015-09-10 14:42:01 +0100

    Adds some element checking functions

--- src/article_tag_checker/core.clj
+++ src/article_tag_checker/core.clj
@@ -1,31 +1,35 @@
 (ns article-tag-checker.core
-	(:require [hickory.core :as hickory]))
+	(:require [hickory.core :as hickory]
+		[clojure.set :as set-ops]))
 
 (def valid-scribe-tags
-	[:code :strong :b :em :i :strike :a :ul :ol :li :blockquote :h2 :sub :sup])
+	#{:code :strong :b :em :i :strike :a :ul :ol :li :blockquote :h2 :sub :sup :p :br})
 
-(def valid-scribe-attributes {:a [:href] :blockquote :class})
+(def valid-scribe-attributes #{:a [:href] :blockquote :class})
 
 (def analysis (atom {}))
 
-(defn extract_tags [html_string]
-	(->> (tree-seq #(not-empty (:content %)) :content (hickory/as-hickory (hickory/parse html_string)))
+(defn valid-flexible-content [tag-set]
+	(set-ops/subset? tag-set valid-scribe-tags))
+
+(defn extract-tags [html-string]
+	(->> (tree-seq #(not-empty (:content %)) :content (hickory/as-hickory (hickory/parse html-string)))
 		(filter #(= :element (:type %)))
 		(map :tag)
 		set
 		))
 
-(defn read_capi [from to]
+(defn read-capi [from to]
 	)
 
-(defn analyse_content [capi_results]
+(defn analyse-content [capi-results]
 	)
 
-(defn write_results [analysed_capi_results]
+(defn write-results [analysed-capi-results]
 	)
 
 (defn foo
   [x]
-  (->> (read_capi)
-  	(analyse_content)
-	(write_results)))
+  (->> (read-capi)
+  	(analyse-content)
+	(write-results)))
--- test/article_tag_checker/core_test.clj
+++ test/article_tag_checker/core_test.clj
@@ -2,9 +2,15 @@
   (:require [clojure.test :refer :all]
             [article-tag-checker.core :refer :all]))
 
-(deftest a-test
+(deftest parsing
   (testing "tag extraction"
-    (let [tags (extract_tags "<p>Hello <a href=\"/test\"></a></p>")]
+    (let [tags (extract-tags "<p>Hello <a href=\"/test\"></a></p>")]
     (and
         (is (some #( = :a %) tags))
         (is (some #( = :p %) tags))))))
+
+(deftest content-checking
+    (testing "valid tags"
+        (is (valid-flexible-content #{:p :a :b}))
+    (testing "invalid tags"
+        (is (not (valid-flexible-content #{:script}))))))
